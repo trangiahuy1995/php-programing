81b1d38b234b9891e86b4fd512fa8f92ab84d27d
Huy <chuzangsin1995@gmail.com>
2017-09-30 02:30:08 -0700

    PHP cu phap

---
+++ cu-phap.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Ví dụ chương trình PHP</title>
+  </head>
+  <body>
+    <?php
+      echo "Ví dụ minh họa lệnh echo! <br>";
+      print "Ví dụ minh họa lệnh print!";
+    ?>
+  </body>
+</html>
