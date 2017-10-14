f691d10bcece5cbf1415c0087f4bc9e59d823372
Huy <chuzangsin1995@gmail.com>
2017-10-14 02:51:16 -0700

    Toan tu

---
+++ toan-tu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<?php
+  $a = 5;
+  $b = 6;
+  $tong = $a + $b;
+  echo "So a = $a<br>";
+  echo "So b = $b<br>";
+  echo "Tong cua a + b = $tong<br>";
+  echo "Cac phep toan con lai thuc hien tuong tu ^^";
+?>
+</body>
+</html>
