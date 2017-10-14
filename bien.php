06736cc2c09695a56b4511c489eff10b83143866
Huy <chuzangsin1995@gmail.com>
2017-10-14 01:06:47 -0700

    bien trong PHP

---
+++ bien.php
@@ -0,0 +1,15 @@
+<?php
+  define("PI",3.14);
+  define("HOTBOY", "Huy dep trai");
+
+  $name = "Tran Gia Huy";
+  $address = "Hue";
+  $old = 21;
+  echo "Ten: $name <br>";
+  echo "****************************<br>";
+  echo "Dia chi: $address <br>";
+  echo "****************************<br>";
+  echo "Tuoi: $old <br>";
+  echo "****************************<br>";
+  echo HOTBOY;
+?>
